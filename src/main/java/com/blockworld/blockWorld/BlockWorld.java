c9081fb52b7262352f236ec2ccfc7b5b62e0a455
Caio "Quintas_" Santiago <13594102+Quinntas@users.noreply.github.com>
2024-08-28 13:42:48 -0300

    Initial commit

---
+++ src/main/java/com/blockworld/blockWorld/BlockWorld.java
@@ -0,0 +1,14 @@
+package com.blockworld.blockWorld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockWorld extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
